7b6ac3bc064725f573fe10d307fae310cee74547
Volker S <mytool.git@d4sweb.de>
2026-05-16 19:43:35 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+from reachy_mini import ReachyMini
+
+# Connect to the running daemon
+with ReachyMini() as mini:
+    print("Connected to Reachy Mini! ")
+    
+    # Wiggle antennas
+    print("Wiggling antennas...")
+    mini.goto_target(antennas=[0.5, -0.5], duration=0.5)
+    mini.goto_target(antennas=[-0.5, 0.5], duration=0.5)
+    mini.goto_target(antennas=[0, 0], duration=0.5)
+
+    print("Done!")
