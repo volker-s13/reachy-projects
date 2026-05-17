1e4578768e8f1a7d33ead69a2b84635bb14b892b
Volker S13 <mytool.git@d4sweb.de>
2026-05-17 17:21:14 +0200

    New movement defined

--- hello.py
+++ hello.py
@@ -2,34 +2,53 @@ from reachy_mini import ReachyMini
 from reachy_mini.utils import create_head_pose
 import time
 
-# Smooth head nod motion
-with ReachyMini() as mini:
+#while True:
+#        taste = input("Press w: ")
+#        if taste == "w":
+#            break
 
-    # Neutral position
-    mini.goto_target(
-        head=create_head_pose(),
-        duration=1.0
-    )
+with ReachyMini() as mini:  
+ 
+    mini.goto_target(head=create_head_pose(), duration=0.5) # Neutral position  
+    time.sleep(0.5)
 
-    time.sleep(0.2)
+    mini.goto_target(head=create_head_pose(z=15, mm=True), duration=0.5) # Move head forward
+    time.sleep(0.5)
 
-    # Nod downward smoothly
-    mini.goto_target(
-        head=create_head_pose(pitch=18, degrees=True),
-        duration=0.45
-    )
+    mini.goto_target(head=create_head_pose(pitch=18, degrees=True), duration=0.5) # Nod downward smoothly
+    time.sleep(0.5)
 
-    # Return smoothly to neutral
-    mini.goto_target(
-        head=create_head_pose(pitch=0, degrees=True),
-        duration=0.55
-    )
+    mini.goto_target(head=create_head_pose(), duration=0.5) # Neutral position  
+    time.sleep(0.5)
 
-    mini.goto_target(
-        head=create_head_pose(z=15, mm=True),
-        duration=1.0
-    )
+    mini.goto_target(head=create_head_pose(z=15, mm=True), duration=0.5) # Move head forward
+    time.sleep(0.5)
 
-    time.sleep(1)
+    mini.goto_target(head=create_head_pose(), duration=0.5) # Neutral position 
+    time.sleep(0.5)
+
+    mini.goto_target(head=create_head_pose(roll=20, degrees=True), duration=0.55) # Tilt head to the right
+    time.sleep(0.5)
+
+    mini.goto_target(head=create_head_pose(), duration=0.5) # Neutral position 
+    time.sleep(0.5)
+
+    mini.goto_target(head=create_head_pose(roll=-20, degrees=True), duration=0.55) # Tilt head to the left
+    time.sleep(0.5)
+
+    mini.goto_target(head=create_head_pose(), duration=0.5) # Neutral position 
+    time.sleep(0.5)
+
+    print("Wiggling antennas...")
+    mini.goto_target(antennas=[0.5, -0.5], duration=0.5)
+    mini.goto_target(antennas=[-0.5, 0.5], duration=0.5)
+    mini.goto_target(antennas=[0, 0], duration=0.5)
+
+    
+    time.sleep(1)  # Wait for the sound to finish playing   
+
+
+
+    
 
 
