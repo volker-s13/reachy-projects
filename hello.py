a1a9343940811cace7642aaa3bcb380cbddd3708
Volker S13 <mytool.git@d4sweb.de>
2026-05-17 09:55:16 +0200

    Geändert in Kopf-Nicken

--- hello.py
+++ hello.py
@@ -1,13 +1,35 @@
 from reachy_mini import ReachyMini
+from reachy_mini.utils import create_head_pose
+import time
 
-# Connect to the running daemon
+# Smooth head nod motion
 with ReachyMini() as mini:
-    print("Connected to Reachy Mini! ")
-    
-    # Wiggle antennas
-    print("Wiggling antennas...")
-    mini.goto_target(antennas=[0.5, -0.5], duration=0.5)
-    mini.goto_target(antennas=[-0.5, 0.5], duration=0.5)
-    mini.goto_target(antennas=[0, 0], duration=0.5)
 
-    print("Done!")
+    # Neutral position
+    mini.goto_target(
+        head=create_head_pose(),
+        duration=1.0
+    )
+
+    time.sleep(0.2)
+
+    # Nod downward smoothly
+    mini.goto_target(
+        head=create_head_pose(pitch=18, degrees=True),
+        duration=0.45
+    )
+
+    # Return smoothly to neutral
+    mini.goto_target(
+        head=create_head_pose(pitch=0, degrees=True),
+        duration=0.55
+    )
+
+    mini.goto_target(
+        head=create_head_pose(z=15, mm=True),
+        duration=1.0
+    )
+
+    time.sleep(1)
+
+
